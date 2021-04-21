7466c2f46a61bca2c4bd24e7d52fbf875c9ff911
Martin Wrede <wrede@targetx.de>
2021-04-21 22:57:33 +0200

    image links 9

--- src/components/image-wrapper.js
+++ src/components/image-wrapper.js
@@ -3,9 +3,9 @@ import React  from "react";
 const image = {
   Design:"https://i.pinimg.com/474x/31/a9/36/31a93614e46f83db6c15cb3d69730630.jpg",
 //  Design: "/media/Interface-Design_E-Learning.jpg",
-  Projekte: "./media/Ausstellung_Erscheinungsbild.jpg",
-  Kooperationen: "/media/Veranstaltungs-Praesentation.jpg",
-  Kontakt: "media/Info-Grafik.jpg",
+  Projekte: "../../public/media/Ausstellung_Erscheinungsbild.jpg",
+  Kooperationen: "./media/Veranstaltungs-Praesentation.jpg",
+  Kontakt: "/media/Info-Grafik.jpg",
 };
 
 class BilderZeigen2 extends React.Component {
@@ -22,8 +22,9 @@ class BilderZeigen2 extends React.Component {
     img.src = image[this.value];
     img.onload = function () {
       document.querySelector(".imagewrapper-quer").style["background-image"] =
-        //    = `url('${img.src}')`
-        `url('${img.src}')`;
+      `url('${img.src}')`;
+          //    = `url('${img.src}')`
+      //     `url('${process.env.PUBLIC_URL + '/media/Info-Grafik.jpg'}')` ;
     };
   }
 
