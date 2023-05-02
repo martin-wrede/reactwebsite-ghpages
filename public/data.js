937672619bc3ca52816f6657b0bba3447ddf1eed
martin-wrede <81897938+martin-wrede@users.noreply.github.com>
2023-05-02 21:54:18 +0200

    Add files via upload
    data.json test

---
+++ public/data.js
@@ -0,0 +1,24 @@
+[
+    {
+        "title": "Home",
+        "gallery_images": 5,
+        "content_h1": "targetx Media-Design",
+        "content_h1_text": "",
+        "content_h2": "1 Plutopay",
+        "content_h2_sub": "Casestudie UX / UI Design / Responsive Website, 5-months Course at CareerFoundry, 2021",
+        "content_h2_image": "Phone-11-flatlay-mockup.jpg",
+        "content_h2_text": "Plutopay is a case study for a fictional banking app for Europe. It offers digital payment methods.",
+        "button1": "Open Case Study"
+    },
+    {
+        "title": "ux-ui-design",
+        "gallery_images": 3,
+        "content_h1": "UX/UI  Design",
+        "content_h1_text": "",
+        "content_h2": "1 Plutopay",
+        "content_h2_sub": "Casestudie UX / UI Design / Responsive Website, 5-months Course at CareerFoundry, 2021",
+        "content_h2_image": "Phone-11-flatlay-mockup.jpg",
+        "content_h2_text": "Plutopay is a case study for a fictional banking app for Europe. It offers digital payment methods.",
+        "button1": "Open Case Study"
+    }
+]
